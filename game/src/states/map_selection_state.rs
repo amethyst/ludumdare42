24fa6e8c3ee651d50e0d15a1cad7c0d814734389
Azriel Hoh <azriel91@gmail.com>
2018-08-11 15:23:12 +1200

    Added button per beatmap to map selection menu.

--- game/src/states/map_selection_state.rs
+++ game/src/states/map_selection_state.rs
@@ -1,8 +1,9 @@
 use amethyst::ecs::prelude::*;
 use amethyst::input::{get_key, is_close_requested, is_key_down};
 use amethyst::renderer::{ElementState, Event, VirtualKeyCode};
+use amethyst::ui::{Anchor, FontAsset, FontHandle, TtfFormat, UiButtonBuilder};
 use amethyst::{GameData, State, StateData, Trans};
-use amethyst_extra::AssetLoader;
+use amethyst_extra::{AssetLoader, AssetLoaderInternal};
 
 use utils::list_beatmaps;
 use GamePlayState;
@@ -10,18 +11,62 @@ use GamePlayState;
 /// Where the player chooses which song to play
 #[derive(Default)]
 pub struct MapSelectionState {
-    beatmaps: Option<Vec<String>>,
+    buttons: Vec<Entity>,
+    font: Option<FontHandle>,
 }
 
 impl MapSelectionState {
     /// Creates a new MapSelectionState.
     pub fn new() -> Self {
-        MapSelectionState { beatmaps: None }
+        MapSelectionState {
+            buttons: Vec::new(),
+            font: None,
+        }
     }
 
     /// Reloads the beatmaps and recreates the menu.
     fn reload_menu(&mut self, world: &mut World) {
-        self.beatmaps = Some(list_beatmaps(&world.read_resource::<AssetLoader>()));
+        let beatmaps = list_beatmaps(&mut world.write_resource::<AssetLoader>());
+        debug!("Beatmaps: {:?}", beatmaps);
+
+        if self.font.is_none() {
+            let font = &mut world
+                .write_resource::<AssetLoader>()
+                .load(
+                    "ui/square.ttf",
+                    TtfFormat,
+                    (),
+                    &mut world.write_resource::<AssetLoaderInternal<FontAsset>>(),
+                    &mut world.write_resource(),
+                    &mut world.read_resource(),
+                ).expect("Failed to load font.");
+            self.font = Some(font.clone());
+        }
+
+        let font = self.font.as_ref().unwrap();
+        self.buttons = beatmaps
+            .iter()
+            .enumerate()
+            .map(|(i, beatmap)| {
+                UiButtonBuilder::new(beatmap, beatmap)
+                    .with_position(0.0, 40.0 + 100.0 * (i as f32 + 1.0))
+                    .with_text_color([0.7; 4])
+                    .with_hover_text_color([1.0; 4])
+                    .with_press_text_color([0.5; 4])
+                    .with_font_size(25.0)
+                    .with_tab_order(i as i32)
+                    .with_anchor(Anchor::TopMiddle)
+                    .with_font(font.clone())
+                    .build_from_world(world)
+            }).collect::<Vec<Entity>>();
+    }
+
+    fn clear_menu(&mut self, world: &mut World) {
+        self.buttons.drain(..).for_each(|button| {
+            world
+                .delete_entity(button)
+                .expect("Failed to delete button.");
+        });
     }
 }
 
@@ -34,6 +79,14 @@ impl<'a, 'b> State<GameData<'a, 'b>> for MapSelectionState {
     fn on_resume(&mut self, mut data: StateData<GameData>) {
         debug!("Resuming MapSelectionState");
         self.reload_menu(&mut data.world);
+    }
+
+    fn on_pause(&mut self, mut data: StateData<GameData>) {
+        self.clear_menu(&mut data.world);
+    }
+
+    fn on_stop(&mut self, mut data: StateData<GameData>) {
+        self.clear_menu(&mut data.world);
     }
 
     fn handle_event(
