a6dcf91e783236498e72c74ee5be193fd5a1ddb4
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-08-12 12:06:41 -0400

    Player movement wip

--- game/src/systems/player_movement.rs
+++ game/src/systems/player_movement.rs
@@ -3,12 +3,15 @@ use amethyst::ecs::{Join, Read, ReadExpect, ReadStorage, Resources, System, Syst
 use amethyst::renderer::{ElementState, Event, VirtualKeyCode};
 use amethyst::input::get_key;
 use amethyst::core::{Time, Transform};
+use amethyst::core::cgmath::{Vector3};
 use amethyst::shrev::{EventChannel, ReaderId};
 
 use data::*;
+use std::collections::VecDeque;
 
 pub struct PlayerMovementSystem {
-    last_beatpoint: Option<BeatPoint>,
+    last_beatpoint: Option<(Vector3<f32>,f64)>,
+    beat_points: Option<VecDeque<(Vector3<f32>,f64)>>,
     /// TODO: Remove and use the timing logic instead
     /// Will break with multiple players (lol)
     in_transit: bool,
@@ -18,6 +21,7 @@ impl PlayerMovementSystem {
     pub fn new() -> Self {
         PlayerMovementSystem {
             last_beatpoint: None,
+            beat_points: None,
             in_transit: false,
         }
     }
@@ -29,34 +33,64 @@ impl<'a> System<'a> for PlayerMovementSystem {
         WriteStorage<'a, Transform>,
         Read<'a, Time>,
         ReadExpect<'a, BeatMap>,
+        ReadStorage<'a, BeatPoint>,
         Read<'a, GameplayResult>,
     );
 
-    fn run(&mut self, (players, mut transforms, time, beatmap, gameplay_result): Self::SystemData) {
+    fn run(&mut self, (players, mut transforms, time, beatmap, beatpoints, gameplay_result): Self::SystemData) {
         let time_to_node_mult = 0.1;
         let rel_time = time.absolute_time_seconds() - beatmap.runtime_start;
 
-        if let Some(front) = beatmap.beat_points.front() {
-            if self.last_beatpoint.is_none() {
-                self.last_beatpoint = Some(front.clone());
-            } else if self.last_beatpoint.as_ref().unwrap() != front && !self.in_transit {
-                self.last_beatpoint = Some(front.clone());
+        info!("1");
+        if self.beat_points.is_none() {
+            info!("2");
+            let mut v = Vec::<(Vector3<f32>,f64)>::new();
+            for (transform,beatpoint) in (&transforms, &beatpoints).join() {
+                info!("boi");
+                v.push((transform.translation.clone(),beatpoint.time));
+            }
+            if !v.is_empty() {
+                v.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
+                self.beat_points = Some(v.into());
+            }
+        } else {
+            info!("3");
+            // FIXME: Skips the first frame
+
+            while !self.beat_points.as_ref().unwrap().is_empty() && self.beat_points.as_ref().unwrap().front().map(|t| t.1) != beatmap.beat_points.front().map(|b| b.time) {
+                info!("4");
+                self.beat_points.as_mut().unwrap().pop_front();
             }
 
-            /*
-            if done transition, last node = current
-            if in transition: transition // breaks on 2+ beatpoint in a single frame (but input too anyway)
+            for (mut transform,_) in (&mut transforms, &players).join() {
+                if self.last_beatpoint.is_none() {
+                    info!("5");
+                    self.last_beatpoint = Some((transform.translation.clone(),0.0));
+                }
 
+                if self.beat_points.as_ref().unwrap().is_empty() {
+                    return;
+                }
 
-            transition = move player pos lerp  lastnode->current  * (deltatime / time_to_node_mult)
-            */
+                // if in transition
+                // if last beatpoint != current, start transitionning
+                if self.last_beatpoint.as_ref() != self.beat_points.as_ref().unwrap().front() {
+                    let trans_time_start = self.last_beatpoint.as_ref().unwrap().1;
+                    let trans_time_stop = self.beat_points.as_ref().unwrap().front().unwrap().1;
 
-            // TODO: finish it, see previous comment for planned algorithm
+                    let trans_duration = (trans_time_stop - trans_time_start) * time_to_node_mult;
 
-            /*if self.last_beatpoint !=
-            let last_beatpoint = ???;*/
+                    let dir = self.beat_points.as_ref().unwrap().front().unwrap().0 - self.last_beatpoint.as_ref().unwrap().0;
+                    let new_pos = self.last_beatpoint.as_ref().unwrap().0 + dir * ((rel_time - trans_time_start) / trans_duration) as f32;
 
-            //if self.last_beatpoint.is_none() ||
+                    // if arrived, last beatpoint = current
+                    if rel_time >= trans_time_start + trans_duration {
+                        self.last_beatpoint = Some(self.beat_points.as_ref().unwrap().front().unwrap().clone());
+                        transform.translation = self.beat_points.as_ref().unwrap().front().unwrap().0.clone();
+                    }
+                }
+
+            }
         }
     }
 }
